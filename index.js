477d03b9fcd9addf77f43fdca9d4eef5dabf7ed1
Nathan Fritz <fritzy@netflint.net>
2014-01-24 23:54:49 -0800

    added index support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var verymodel = require('verymodel');
 var uuid = require('node-uuid');
+var async = require('async');
 
 function makeModelLevely(mf) {
     mf.addDefinition({
@@ -48,9 +49,62 @@ function makeModelLevely(mf) {
         });
     };
 
+    mf.getByIndex = function (field, value, callback) {
+        if (this.definition[field].hasOwnProperty('index')) {
+            this.options.db.get(this.definition[field].index, function (err, index) {
+                if (err || !index) index = {};
+                if (index.hasOwnProperty(value)) {
+                    this.options.db.get(index[value], function (err, result) {
+                        var obj;
+                        if (!err) {
+                            obj = mf.create(result);
+                            obj.key = index[value];
+                            callback(err, obj);
+                        } else {
+                            callback(err);
+                        }
+                    }.bind(this));
+                } else {
+                    callback("no index for value");
+                }
+            }.bind(this));
+        } else {
+            callback("field does not have index");
+        }
+    };
+
     mf.extendModel({
         save: function (callback) {
-            this.__verymeta.db.put(this.key, this.toJSON(), callback);
+            async.waterfall([
+                function (acb) {
+                    this.__verymeta.db.put(this.key, this.toJSON(), acb);
+                }.bind(this),
+                function (acb) {
+                    Object.keys(this.__verymeta.defs).forEach(function (field) {
+                        var ikey;
+                        if (this.__verymeta.defs[field].hasOwnProperty('index')) {
+                            ikey = this.__verymeta.defs[field].index;
+                            this.__verymeta.db.get(ikey, function (err, obj) {
+                                var objkeys;
+                                if (err || !obj) {
+                                    obj = {};
+                                }
+                                objkeys = Object.keys(obj);
+                                for (var kidx in objkeys) {
+                                    if (obj[objkeys[kidx]] === this.key) {
+                                        delete obj[objkeys[kidx]];
+                                    }
+                                }
+                                obj[this[field]] = this.key;
+                                this.__verymeta.db.put(ikey, obj, acb);
+                            }.bind(this));
+                        }
+                    }.bind(this));
+                }.bind(this),
+            ],
+            function (err) {
+                callback(err);
+            });
         },
         delete: function (callback) {
             this.__verymeta.db.del(this.key, callback);
--- test.js
+++ test.js
@@ -7,22 +7,29 @@ var Person = new VeryLevelModel({
     last_name: {},
     full_name: {derive: function () {
         return this.first_name + ' ' + this.last_name;
-        }, private: false},
+    }, private: false},
     experience: {},
     title: {},
+    twitter: {index: "twitter_index"}
 }, {db: db, prefix: 'person!'});
 
 
 var gar = Person.create({
     first_name: 'Michael', 
     last_name: 'Garvin',
+    twitter: '@gar',
 });
 
 
 gar.save(function (err) {
+    console.log("save?");
     Person.load(gar.key, function (err, person) {
         console.log(person.toJSON());
         console.log(person.key);
+    });
+    Person.getByIndex('twitter', '@gar', function (err, person) {
+        console.log("got twitter person");
+        console.log(person.toJSON());
     });
 });
 
